b2d94a17cf4b384ac3e9f3ef9d454cdae9c72798
Anastasiia Smirnova <smirnovaan47@gmail.com>
2024-07-08 23:29:40 +0300

    Edited HW_03.py

--- HW_03/HW_03.py
+++ HW_03/HW_03.py
@@ -1,2 +1,24 @@
 import sys
-import pathlib
+
+from pathlib import Path
+
+from colorama import Fore, Back, Style
+
+path_from_terminal = sys.argv
+
+directory_path = Path(path_from_terminal[1])
+
+
+def print_dir_content(current_dir=Path('.'), shift=0, extensions_colors=None):
+    if extensions_colors is None:
+        extensions_colors = {'.py': Fore.MAGENTA, '.png': Fore.MAGENTA, '.jpeg': Fore.MAGENTA, '.txt': Fore.MAGENTA }
+
+    for path in current_dir.iterdir():
+        if path.is_dir():
+            print(' ' * shift + f'- {path.name}')
+            print_dir_content(path, shift + 2)
+        else:
+            styles = extensions_colors[path.suffix]
+            print(' ' * shift + styles + path.name + Style.RESET_ALL)
+
+print(print_dir_content(directory_path))
